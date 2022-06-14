1a90a212207485c1337e5a500b0aef9f780ef021
Brian Emilius <be@rts.dk>
2022-06-14 13:12:08 +0200

    add payment types model and controller

---
+++ controllers/paymenttypes.js
@@ -0,0 +1,27 @@
+import User, { PaymentType } from "../models/usermodel.js";
+
+export async function createPaymentType(request, response) {
+	const newPaymentType = new PaymentType()
+	newPaymentType.type = request.body.type
+	if (request.body.creditcard) {
+		newPaymentType.creditcard = {
+			name: request.body.creditcard.name,
+			number: request.body.creditcard.number,
+			exp: request.body.creditcard.exp,
+			cvc: request.body.creditcard.cvc
+		}
+	}
+	newPaymentType.paypal = request.body.paypal
+
+	try {
+		const result = await newPaymentType.save()
+		const id = request.params.id
+
+		await User.findByIdAndUpdate(id, { paymentTypes: [result._id] })
+
+		response.status(201).json(result)
+	} catch (error) {
+		console.log(error.message)
+		response.status(500).end()
+	}
+}
--- router.js
+++ router.js
@@ -1,4 +1,5 @@
 import express from "express"
+import { createPaymentType } from "./controllers/paymenttypes.js"
 import { getAllProducts, createProduct, getSingleProduct, deleteProduct} from "./controllers/products.js"
 import { createToken } from "./controllers/token.js"
 import { createUser } from "./controllers/users.js"
@@ -12,6 +13,9 @@ Router.delete("/api/v1/products/:query", checkHeader, checkToken, deleteProduct)
 
 Router.post("/api/v1/users", createUser)
 
+Router.post("/api/v1/users/:id/payment-methods", createPaymentType)
+
 Router.post("/auth/token", createToken)
+
 
 export default Router
